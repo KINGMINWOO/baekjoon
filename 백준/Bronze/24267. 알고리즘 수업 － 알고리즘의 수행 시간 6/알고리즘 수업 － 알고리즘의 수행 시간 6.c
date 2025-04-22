bfacdcda80884576a6f75dea2b73c3c0d9648628
KINGMINWOO <110845983+KINGMINWOO@users.noreply.github.com>
2025-04-22 11:08:33 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+
+{
+    
+    long long n;
+    scanf("%lld", &n);
+    
+    printf("%lld\n", n * (n - 1) * (n - 2) / 3 / 2);
+    printf("%d", 3);
+        
+    return 0;
+}
