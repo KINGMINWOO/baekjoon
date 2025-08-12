f20b1561a3ebd0ddce3b00338f938637f8780b6d
KINGMINWOO <110845983+KINGMINWOO@users.noreply.github.com>
2025-08-12 18:20:21 +0900

    [Silver II] Title: 싸이버개강총회, Time: 168 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main() {
+	string S, E, Q;
+
+	cin >> S >> E >> Q;
+
+	string time, name;
+	set<string> set;
+	int cnt = 0;
+	while (!cin.eof()) {
+		cin >> time >> name;
+
+		if (S >= time) {
+			set.emplace(name);
+		}
+		
+		if (E <= time && Q >= time) {
+			if (set.count(name)){
+				set.erase(name);
+				cnt++;
+			}
+		}
+	}
+	/*for (int i = 0; i < 17; i++) {
+		cin >> time >> name;
+
+		if (S >= time) {
+			set.emplace(name);
+		}
+
+		if (E <= time && Q >= time) {
+			if (set.count(name)) {
+				cnt++;
+				set.erase(name);
+				cout << name << '\n';
+			}
+		}
+	}*/
+	cout << cnt;
+}
